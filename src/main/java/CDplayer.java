c4d3186cfcacbbd3c65dfb8c6ffcc8bfc708f255
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-16 12:08:21 +0100

    CDplayer complete

--- src/main/java/CDplayer.java
+++ src/main/java/CDplayer.java
@@ -1,20 +1,47 @@
+import java.util.ArrayList;
+
 public class CDplayer extends Component {
 
     private int numOfCDs;
+    private CD currentCD;
+
+    private ArrayList<CD> cds;
 
     public CDplayer(String make, String model, int numOfCDs){
         super(make, model);
         this.numOfCDs = numOfCDs;
+        this.cds = new ArrayList<>();
+        this.currentCD = null;
     }
 
     public int getNumOfCDs() {
         return numOfCDs;
     }
 
-    public void cdIn(CD cd){
-
+    public ArrayList<CD> getCds() {
+        return cds;
     }
 
+    public void cdIn(CD cd){
+        if (cds.size() < numOfCDs){
+            cds.add(cd);
+            currentCD = cd;
+        }
+    }
 
+    public void eject(CD cd){
+        if (cds.contains(cd)) {
+            cds.remove(cd);
+        }
+    }
 
+    public CD getCurrentCD() {
+        return currentCD;
+    }
+
+    public void changeCD(CD cd) {
+        if (cds.contains(cd)) {
+            currentCD = cd;
+        }
+    }
 }
--- src/test/java/CDplayerTest.java
+++ src/test/java/CDplayerTest.java
@@ -1,2 +1,83 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CDplayerTest {
+    
+    CD cd1;
+    CD cd2;
+    CDplayer player;
+    
+    @Before
+    public void before(){
+        cd1 = new CD("TestCD1", 10);
+        cd2 = new CD("TestCD2", 8);
+        player = new CDplayer("Sony", "CD400", 2);
+    }
+
+    @Test
+    public void getMake() {
+        assertEquals("Sony", player.getMake());
+    }
+
+    @Test
+    public void getModel(){
+        assertEquals("CD400", player.getModel());
+    }
+
+    @Test
+    public void getVolume(){
+        assertEquals(5, player.getVolume());
+    }
+
+    @Test
+    public void volumeUp(){
+        player.volumeUp();
+        assertEquals(6, player.getVolume());
+    }
+
+    @Test
+    public void volumeDn(){
+        player.volumeDn();
+        assertEquals(4, player.getVolume());
+    }
+
+    @Test
+    public void numberOfCDs(){
+        assertEquals(2, player.getNumOfCDs());
+    }
+
+    @Test
+    public void cdIn(){
+        player.cdIn(cd1);
+        assertEquals(1, player.getCds().size());
+    }
+
+    @Test
+    public void eject(){
+        player.cdIn(cd1);
+        player.cdIn(cd2);
+        assertEquals(2, player.getCds().size());
+        player.eject(cd1);
+        assertEquals(1, player.getCds().size());
+    }
+
+    @Test
+    public void getCurrentCD(){
+        player.cdIn(cd2);
+        player.cdIn(cd1);
+        assertEquals(2, player.getCds().size());
+        assertEquals(cd1, player.getCurrentCD());
+    }
+
+    @Test
+    public void changeCD(){
+        player.cdIn(cd2);
+        player.cdIn(cd1);
+        assertEquals(2, player.getCds().size());
+        assertEquals(cd1, player.getCurrentCD());
+        player.changeCD(cd2);
+        assertEquals(cd2, player.getCurrentCD());
+    }
 }
