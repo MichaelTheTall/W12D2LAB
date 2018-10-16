e55c8fa1cd3f41435940920088c0b03f62ee068c
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-16 11:53:57 +0100

    CD class

---
+++ src/main/java/CDplayer.java
@@ -0,0 +1,20 @@
+public class CDplayer extends Component {
+
+    private int numOfCDs;
+
+    public CDplayer(String make, String model, int numOfCDs){
+        super(make, model);
+        this.numOfCDs = numOfCDs;
+    }
+
+    public int getNumOfCDs() {
+        return numOfCDs;
+    }
+
+    public void cdIn(CD cd){
+
+    }
+
+
+
+}
