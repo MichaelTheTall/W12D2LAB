374b84011c19fc0df871b7c16b490e3d29f64ed2
MichaelTheTall <michaeljhnewman@gmail.com>
2018-10-16 13:18:03 +0100

    MVP complete

--- src/main/java/Stereo.java
+++ src/main/java/Stereo.java
@@ -39,6 +39,82 @@ public class Stereo {
         return aux;
     }
 
+    public void tuneRadio(Station newStation){
+        radio.tune(newStation);
+    }
+
+    public Station radioStation(){
+        return radio.getStation();
+    }
+
+    public int radioVol(){
+        return radio.getVolume();
+    }
+
+    public void radioUp(){
+        radio.volumeUp();
+    }
+
+    public void radioDn(){
+        radio.volumeDn();
+    }
+
+    public int auxVol(){
+        return aux.getVolume();
+    }
+
+    public void auxUp(){
+        aux.volumeUp();
+    }
+
+    public void auxDn(){
+        aux.volumeDn();
+    }
+
+    public int cdVol(){
+        return cdplayer.getVolume();
+    }
+
+    public void cdUp(){
+        cdplayer.volumeUp();
+    }
+
+    public void cdDn(){
+        cdplayer.volumeDn();
+    }
+
+    public void insertCD(CD cd){
+        cdplayer.cdIn(cd);
+    }
+
+    public void ejectCD(CD cd){
+        cdplayer.eject(cd);
+    }
+
+    public void changeCD(CD cd){
+        cdplayer.changeCD(cd);
+    }
+
+    public String cdplay(){
+        return cdplayer.play();
+    }
+
+    public String cdpause(){
+        return cdplayer.pause();
+    }
+
+    public String cdstop(){
+        return cdplayer.stop();
+    }
+
+    public String cdnext(){
+        return cdplayer.next();
+    }
+
+    public String cdprevious(){
+        return cdplayer.previous();
+    }
+
 
 
 }
